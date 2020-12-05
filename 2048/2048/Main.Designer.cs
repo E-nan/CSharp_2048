009f9aa7cb5fe775fde8bbd8e22ce2f8e9390ce6
E-nan <be_victorious@naver.com>
2020-12-05 16:10:21 +0900

    기본 이벤트 생성

--- 2048/2048/Main.Designer.cs
+++ 2048/2048/Main.Designer.cs
@@ -29,29 +29,31 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tsl_main = new System.Windows.Forms.TableLayoutPanel();
             this.ts_topBar = new System.Windows.Forms.ToolStrip();
             this.tsb_exit = new System.Windows.Forms.ToolStripButton();
             this.tsb_resize = new System.Windows.Forms.ToolStripButton();
             this.tsb_hide = new System.Windows.Forms.ToolStripButton();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.tlp_numberBoard = new System.Windows.Forms.TableLayoutPanel();
+            this.tsl_main.SuspendLayout();
             this.ts_topBar.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // tsl_main
             // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.ts_topBar, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.45963F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.54037F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(473, 674);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.tsl_main.ColumnCount = 1;
+            this.tsl_main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tsl_main.Controls.Add(this.ts_topBar, 0, 0);
+            this.tsl_main.Controls.Add(this.tlp_numberBoard, 0, 2);
+            this.tsl_main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tsl_main.Location = new System.Drawing.Point(0, 0);
+            this.tsl_main.Name = "tsl_main";
+            this.tsl_main.RowCount = 3;
+            this.tsl_main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tsl_main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.34515F));
+            this.tsl_main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75.65485F));
+            this.tsl_main.Size = new System.Drawing.Size(473, 674);
+            this.tsl_main.TabIndex = 0;
             // 
             // ts_topBar
             // 
@@ -77,6 +79,7 @@
             this.tsb_exit.Name = "tsb_exit";
             this.tsb_exit.Size = new System.Drawing.Size(23, 22);
             this.tsb_exit.Text = "toolStripButton1";
+            this.tsb_exit.Click += new System.EventHandler(this.tsb_exit_Click);
             // 
             // tsb_resize
             // 
@@ -87,6 +90,7 @@
             this.tsb_resize.Name = "tsb_resize";
             this.tsb_resize.Size = new System.Drawing.Size(23, 22);
             this.tsb_resize.Text = "toolStripButton2";
+            this.tsb_resize.Click += new System.EventHandler(this.tsb_resize_Click);
             // 
             // tsb_hide
             // 
@@ -97,21 +101,43 @@
             this.tsb_hide.Name = "tsb_hide";
             this.tsb_hide.Size = new System.Drawing.Size(23, 22);
             this.tsb_hide.Text = "toolStripButton3";
+            this.tsb_hide.Click += new System.EventHandler(this.tsb_hide_Click);
+            // 
+            // tlp_numberBoard
+            // 
+            this.tlp_numberBoard.ColumnCount = 4;
+            this.tlp_numberBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp_numberBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp_numberBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp_numberBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp_numberBoard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlp_numberBoard.Location = new System.Drawing.Point(3, 185);
+            this.tlp_numberBoard.Name = "tlp_numberBoard";
+            this.tlp_numberBoard.RowCount = 4;
+            this.tlp_numberBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp_numberBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp_numberBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp_numberBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp_numberBoard.Size = new System.Drawing.Size(467, 486);
+            this.tlp_numberBoard.TabIndex = 1;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(473, 674);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tsl_main);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Main_KeyDown);
+            this.tsl_main.ResumeLayout(false);
+            this.tsl_main.PerformLayout();
             this.ts_topBar.ResumeLayout(false);
             this.ts_topBar.PerformLayout();
             this.ResumeLayout(false);
@@ -120,11 +146,12 @@
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tsl_main;
         private System.Windows.Forms.ToolStrip ts_topBar;
         private System.Windows.Forms.ToolStripButton tsb_exit;
         private System.Windows.Forms.ToolStripButton tsb_resize;
         private System.Windows.Forms.ToolStripButton tsb_hide;
+        private System.Windows.Forms.TableLayoutPanel tlp_numberBoard;
     }
 }
 
--- 2048/2048/Main.cs
+++ 2048/2048/Main.cs
@@ -16,5 +16,54 @@ namespace _2048
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// 폼 최소화 이벤트
+        /// </summary>
+        private void HideForm()
+        {
+
+        }
+
+        /// <summary>
+        /// 폼 이전 크기로, 최대화 이벤트
+        /// </summary>
+        private void ResizeForm()
+        {
+
+        }
+
+        /// <summary>
+        /// 폼 나가기 이벤트
+        /// </summary>
+        private void ExitForm()
+        {
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_KeyDown(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine(e.KeyValue);
+        }
+
+        private void tsb_hide_Click(object sender, EventArgs e)
+        {
+            HideForm();
+        }
+
+        private void tsb_resize_Click(object sender, EventArgs e)
+        {
+            ResizeForm();
+        }
+
+        private void tsb_exit_Click(object sender, EventArgs e)
+        {
+            ExitForm();
+        }
     }
 }
